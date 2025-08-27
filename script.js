c0d8dfa1a2429521b7770cfcd50dc4aff0e3189c
John <18509053+frogsforjp@users.noreply.github.com>
2025-08-27 18:58:25 -0400

    refactor: simplify hover functions for readability
    I reduced the number of functions used to increment opacity from 3 to 1.
    This also allowed me to remove 5 CSS classes for opacity and change to
    using an inline style instead.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('.grid-container');
-const tile = document.querySelectorAll('.tile');
+const gridButton = document.querySelector('.new-grid-button');
 
 function createGrid(tilesPerSide) {
     grid.style.setProperty('--tiles-per-side', tilesPerSide);
@@ -7,55 +7,22 @@ function createGrid(tilesPerSide) {
     const totalTiles = tilesPerSide * tilesPerSide;
     for (let i = totalTiles; i > 0; i--) {
         const tile = document.createElement('div');
-        tile.classList.add('tile', 'opacity0');
+        tile.classList.add('tile');
         grid.appendChild(tile);
     }
 }
 
-createGrid(16);
-
-function colorHoveredTiles(event) {
-    if (event.target.classList.contains('tile')) {
-        increaseOpacityClass(event.target);
+function increaseOpacity(event) {
+    let tile = event.target;
+    if (tile.classList.contains('tile')) {
+        let currentOpacity = parseFloat(tile.style.opacity) || 0.1;
+        newOpacity = Math.min(currentOpacity + 0.1, 1);
+        tile.style.backgroundColor = 'black';
+        tile.style.opacity = newOpacity;
     }
 }
 
-function increaseOpacityClass(tile) {
-    // I chose to increment the opacity to get some practice with adding and
-    // removing classes with an array. I've realized it doesn't look as good as
-    // going from 0 to 100 opacity so I might remove it or add an option to
-    // toggle it on and off later.
-    const opacityClasses = [
-        'opacity0', 
-        'opacity20', 
-        'opacity40', 
-        'opacity60', 
-        'opacity80', 
-        'opacity100'
-    ];
-    const tileOpacityClass = getTileOpacityClass(tile, opacityClasses);
-    const tileIndex = opacityClasses.indexOf(tileOpacityClass);
-    
-    tile.classList.remove(...opacityClasses);
-
-    if (tileIndex < 5) {
-        tile.classList.add(opacityClasses[tileIndex + 1]);
-    } else {
-        tile.classList.add(opacityClasses[5]);
-    }
-}
-
-function getTileOpacityClass(tile, arrayOfClasses) {
-    for (const className of arrayOfClasses) {
-        if (tile.classList.contains(className)) {
-            return className;
-        }
-    }
-}
-
-grid.addEventListener('mouseover', colorHoveredTiles);
-
-function promptGridTiles(callbackFunction) {
+function promptGridTiles() {
     const tilesPerSide = prompt("Choose how many tiles per side (Max 100)", "");
     const tilesPerSideInteger = parseInt(tilesPerSide);
 
@@ -67,5 +34,7 @@ function promptGridTiles(callbackFunction) {
     }
 }
 
-const gridButton = document.querySelector('.new-grid-button');
+createGrid(16);
+
+grid.addEventListener('mouseover', increaseOpacity);
 gridButton.addEventListener('click', promptGridTiles);

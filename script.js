3a7d9e1e72edb7d0490e89dbadc727c688de9e11
John <18509053+frogsforjp@users.noreply.github.com>
2025-08-28 22:14:14 -0400

    feat: Add buttons to change draw mode or clear

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
+let currentMode = 'fill';
+let currentColor = 'black';
+let currentSize = 16;
+
+
 const grid = document.querySelector('.grid-container');
 const gridButton = document.querySelector('.new-grid-button');
 
-function createGrid(tilesPerSide) {
-    grid.style.setProperty('--tiles-per-side', tilesPerSide);
+const colorBtn = document.querySelector('.color');
+const rainbowBtn = document.querySelector('.rainbow');
+const shadingBtn = document.querySelector('.shade');
+const eraserBtn = document.querySelector('.erase');
+const clearBtn = document.querySelector('.clear');
+const toggleBtn = document.querySelector('.toggle');
 
+function createGrid(tilesPerSide) {
+    // remove this line when you add a slider to update size
+    currentSize = tilesPerSide;
+    grid.replaceChildren();
     const totalTiles = tilesPerSide * tilesPerSide;
+
     for (let i = totalTiles; i > 0; i--) {
         const tile = document.createElement('div');
-        tile.classList.add('tile');
         grid.appendChild(tile);
+        tile.style.cssText = `
+            opacity: 0;
+            width: calc(100% / ${tilesPerSide});
+            height: calc(100% / ${tilesPerSide});
+        `;
+        
+        // event listeners for painting here
+        tile.addEventListener('mouseover', Draw);
     }
 }
 
-function increaseOpacity(event) {
+function Draw(event) {
     let tile = event.target;
-    if (tile.classList.contains('tile')) {
-        let currentOpacity = parseFloat(tile.style.opacity) || 0.1;
-        newOpacity = Math.min(currentOpacity + 0.1, 1);
-        tile.style.backgroundColor = 'black';
-        tile.style.opacity = newOpacity;
+
+    if (currentMode == 'fill') {
+        tile.style.opacity = 1;
+        tile.style.backgroundColor = currentColor;
+    } else if (currentMode == 'rainbow') {
+        tile.style.opacity = 1;
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        tile.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    } else if (currentMode == 'shade') {
+        let currentOpacity = parseFloat(tile.style.opacity);
+        if (currentOpacity < 1) {
+            currentOpacity += 0.1;
+            tile.style.opacity = currentOpacity;
+            tile.style.backgroundColor = currentColor;
+        }
+    } else if (currentMode == 'erase') {
+        tile.style.opacity = 0;
+        tile.style.backgroundColor = 'white';
     }
 }
 
@@ -29,12 +65,27 @@ function promptGridTiles() {
     if (!Number.isInteger(tilesPerSideInteger) || tilesPerSide <= 0 || tilesPerSide > 100) {
         alert("Please enter a valid number from 1-100");
     } else {
-        grid.replaceChildren();
         createGrid(tilesPerSide);
     }
 }
 
-createGrid(16);
+function clearBoard() {
+    createGrid(currentSize);
+}
 
-grid.addEventListener('mouseover', increaseOpacity);
+function updateCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
+rainbowBtn.addEventListener('click', () => updateCurrentMode('rainbow'));
+eraserBtn.addEventListener('click', () => updateCurrentMode('erase'));
+shadingBtn.addEventListener('click', () => updateCurrentMode('shade'));
+colorBtn.addEventListener('click', () => updateCurrentMode('fill'));
+// clearBoard works with no argument because it's waiting for the event as a
+// argument which it receives on a click. That's why I needed to use anonymous
+// functions above
+clearBtn.addEventListener('click', clearBoard);
+
 gridButton.addEventListener('click', promptGridTiles);
+
+createGrid(currentSize);
